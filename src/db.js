b2e0c50ede54d5ed9889c76f42c0243aac79ecc7
Flávio Vinícius Martins dos Santos <vinicius0809@gmail.com>
2022-08-16 11:54:18 -0300

    Change appId correctly

--- src/db.js
+++ src/db.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDLB27nWPHbrSNTwBNfaLit1N8Fn-me7j4",
   authDomain: "open-banking-open-source.firebaseapp.com",
+  databaseURL: "https://open-banking-open-source-default-rtdb.firebaseio.com",
   projectId: "open-banking-open-source",
   storageBucket: "open-banking-open-source.appspot.com",
-  databaseURL: "https://open-banking-open-source-default-rtdb.firebaseio.com",
   messagingSenderId: "691278522719",
-  appId: "1:691278522719:web:5e32f234fb907d76dbaeea",
-  measurementId: "G-47842PXB6F"
+  appId: "1:691278522719:web:0b802ab248c6876adbaeea",
+  measurementId: "G-54CBMJRMCP"
   };
 const app = firebase.initializeApp(firebaseConfig);
 // Get a Firestore instance
